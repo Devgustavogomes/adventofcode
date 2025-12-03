bdaffea64bee9f440f2dd2ccde3bdfddeedda145
DevGustavo <devgustavogomes31@gmail.com>
2025-12-03 14:07:27 -0300

    quest 4

--- Quest4.js
+++ Quest4.js
@@ -1,11 +1,11 @@
 const raw = document.querySelector("pre").textContent;
 const inputs = raw.split(",");
 const invalidIds = [];
-
 let s, mid, left, right;
 
 for (let i = 0; i < inputs.length; i++) {
   const [initStr, finishStr] = inputs[i].split("-");
+
   const init = Number(initStr);
   const finish = Number(finishStr);
 
@@ -13,18 +13,17 @@ for (let i = 0; i < inputs.length; i++) {
     s = j.toString();
 
     for (let size = 1; size < s.length; size++) {
-      if (s.length % size !== 0) continue;
-
       const block = s.slice(0, size);
       const repetitions = s.length / size;
 
       if (block.repeat(repetitions) === s && repetitions >= 2) {
         invalidIds.push(j);
+        break;
       }
     }
   }
 }
-console.log(invalidIds);
+
 const totalInvalids = invalidIds.reduce(
   (totalValue, currentValue) => totalValue + currentValue,
   0
